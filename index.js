d24198c0a7f24ad53aed9b682b92948521833aa7
TatraiDominik <tatraidominikoszkar@turr.hu>
2024-11-11 09:05:15 +0100

    elindult a backend(?)

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+var session = require('express-session');
 
+const app = express();
+var port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/assets', express.static('assets'));
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}...`);
+});
